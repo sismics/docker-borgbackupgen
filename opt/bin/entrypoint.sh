41e7ddbce929b4f9be6d5637f5621f79c72720cf
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2016-12-28 10:47:05 +0100

    Add prefix to run Borgbackup last

--- opt/bin/entrypoint.sh
+++ opt/bin/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-gen -only-exposed -watch /etc/dockergen/dockergen.borg.tmpl /etc/backup.d/dockergen.borg
+docker-gen -only-exposed -watch /etc/dockergen/dockergen.borg.tmpl /etc/backup.d/99-dockergen.borg
